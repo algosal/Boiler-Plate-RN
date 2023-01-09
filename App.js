6d825505536b133be4f79cbedcc8ec8da1f38977
algosal <pfc.salman@gmail.com>
2023-01-09 09:27:54 -0500

    First Commit for the Boiler Plate of React Native. Extra stuff from install taken out

---
+++ App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {Colors,} from 'react-native/Libraries/NewAppScreen';
+
+
+
+
+function App(){
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar/>  
+        <View style={styles.AppBG}><Text style={styles.sectionTitle}>Boiler Plate by Salman</Text></View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign:'center',
+    color:'blue', 
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  AppBG:{
+    backgroundColor:'transparent'
+  },
+});
+
+export default App;
